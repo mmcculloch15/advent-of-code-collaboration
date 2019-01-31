b3cd0e14d30667127bc314f16e9755ce176e0bab
Mitchell McCulloch <mitchell.mcculloch@rangle.io>
2019-01-30 22:12:38 -0500

    Refactored with more regexs, added some comments and removed commented out code

--- day4/day4.js
+++ day4/day4.js
@@ -9,16 +9,18 @@ const records = fs
 const guards = []
 
 function getMinuteFromRecord(record) {
-  const timestamp = record.slice(1).split(']')[0]
-  return parseInt(timestamp.slice(timestamp.length - 2))
+  //Extract the minutes section from the timestamp by matching digits after a colon 
+  const regex = /:(\d+)/
+  const timestamp = record.match(regex)[1]
+  return parseInt(timestamp)
 }
 
 function part1(records) {
   let currentGuard, currentGuardIndex, theMinuteTheyFellAsleep, theMinuteTheyWokeUp
   for (record of records) {
     if (record.includes('Guard')) {
-      currentGuard = record.split('#')[1].split(' ')[0]
-      console.log(`Current guard ${currentGuard}`)
+      //Using a regex, match any number of digits after a # character and assign it as the currentGuard
+      const currentGuard = record.match(/#(\d+)/)[1]
 
       if (!guards.find(guard => guard.id === currentGuard)) {
         guards.push({
@@ -37,8 +39,6 @@ function part1(records) {
       theMinuteTheyWokeUp = getMinuteFromRecord(record)
       const minutesAsleep = theMinuteTheyWokeUp - theMinuteTheyFellAsleep
 
-
-
       for (let i = theMinuteTheyFellAsleep; i < (theMinuteTheyFellAsleep + minutesAsleep); i++) {
         //if the guard has not fallen asleep on this minute bofore, add it to the minutesAsleep object and set it to 1
         if (!guards[currentGuardIndex].minutesAsleep[i]) {
@@ -50,59 +50,30 @@ function part1(records) {
       }
 
       guards[currentGuardIndex].totalMinutesAsleep += minutesAsleep
-
-
-      // for (let i = theMinuteTheyFellAsleep; i < (theMinuteTheyFellAsleep + minutesAsleep); i++) {
-      //   if (!guards.find(guard => guard.id === currentGuard).minutesAsleep[i]) guards[currentGuard].minutesAsleep[i] = 1
-      //   else guards[currentGuard].minutesAsleep[i]++
-      // }
-      // //increment the guards time asleep by minutesAsleep? guards[currentGuard].totalTimeAsleep++
-      // guards[currentGuard].totalMinutesAsleep += minutesAsleep
     }
   }
 
   //At this point, the guard Object will contain each guard, their individual minutes asleep, and their totalTimeAsleep
-
-  //Compare totalTimeAsleep to find the guard that slept the most
-
-  //Object.entries(guards).map(guard => { console.log(guard) })
+  //Sort the guards array by the totalMinutesAsleep variable to find the guard that slept the most
   const sortedGuards = guards.sort((guard1, guard2) => {
+    //If guard1 slept more than guard2, move guard1 to an index below guard2
     if (guard1.totalMinutesAsleep < guard2.totalMinutesAsleep) return 1
     else if (guard1.totalMinutesAsleep === guard2.totalMinutesAsleep) return 0
     else return -1
   })
 
   const correctGuard = sortedGuards[0].id
-  //const bestMinute = Math.max(...Object.values(sortedGuards[0].minutesAsleep))
-
-
-
   const bestMinute = Object.entries(sortedGuards[0].minutesAsleep).reduce((bestMinute, currentMinute) => {
     if (currentMinute[1] > bestMinute.value) return { minute: currentMinute[0], value: currentMinute[1] }
     else return bestMinute
-    //if (currentMinute > bestMinute.value) return { index: currentIndex, value: currentMinute }
   }, {
       minute: 0,
       value: 0
     })
 
 
-  console.log(`best minute: ${bestMinute.minute}, target guard: ${sortedGuards[0].id}`)
+  //console.log(`best minute: ${bestMinute.minute}, target guard: ${sortedGuards[0].id}`)
   console.log(`answer: ${(bestMinute.minute) * sortedGuards[0].id}`)
-
-  //For that guard, find the minute that has the highest occurrences of being asleep
-
-  //   let numOfMaxAsleepTime = guards[0]
-  // console.log(Array.from(guards => guards))      
-  //console.log(guards)
-
-  // Object.keys(guards).forEach(guard => {
-  //   guardSleepTime[guard] = Object.values(guards[guard].minutesAsleep).reduce((totalMinutesAsleep, val) => totalMinutesAsleep += val, 0)
-  // })
-
-  // const sortedGuardTime = Object.values(guardSleepTime).sort()
-  //console.log(sortedGuardTime[sortedGuardTime.length - 1])
-  //then look at their individual minutesAsleep object to find the minute they were asleep the most
 }
 
 part1(records)
