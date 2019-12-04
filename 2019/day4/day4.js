bbf0f26248e92172874af9128636513807501bc9
Mitchell McCulloch <mitchell.mcculloch@rangle.io>
2019-12-04 13:07:18 -0500

    removed unnecessary named booleans

--- 2019/day4/day4.js
+++ 2019/day4/day4.js
@@ -4,41 +4,40 @@ const rangeEnd = 562041
 let validPasswords = 0
 
 const hasAscendingNumbers = password =>
-  password
-    .toString()
-    .split('')
-    .sort()
-    .join() ==
-  password
-    .toString()
-    .split('')
-    .join()
+	password
+		.toString()
+		.split('')
+		.sort()
+		.join() ==
+	password
+		.toString()
+		.split('')
+		.join()
 
 const hasExactlyTwoAdjacent = (passwordString, i) => {
-  let valid = false
-  if (!passwordString[i - 1] || passwordString[i - 1] != passwordString[i]) {
-    if (!passwordString[i + 2] || passwordString[i + 2] != passwordString[i]) {
-      valid = true
-    }
-  }
-  return valid
+	if (!passwordString[i - 1] || passwordString[i - 1] != passwordString[i]) {
+		if (!passwordString[i + 2] || passwordString[i + 2] != passwordString[i]) {
+			return true
+		}
+	}
+	return false
 }
 
 const hasMatchingAdjacentDigits = password => {
-  const passwordString = String(password)
+	const passwordString = String(password)
 
-  for (let i = 0; i < passwordString.length - 1; i++) {
-    if (passwordString[i] === passwordString[i + 1]) {
-      if (hasExactlyTwoAdjacent(passwordString, i)) return true
-    }
-  }
-  return false
+	for (let i = 0; i < passwordString.length - 1; i++) {
+		if (passwordString[i] === passwordString[i + 1]) {
+			if (hasExactlyTwoAdjacent(passwordString, i)) return true
+		}
+	}
+	return false
 }
 
 for (let i = rangeStart; i <= rangeEnd; i++) {
-  if (hasAscendingNumbers(i) && hasMatchingAdjacentDigits(i)) {
-    validPasswords++
-  }
+	if (hasAscendingNumbers(i) && hasMatchingAdjacentDigits(i)) {
+		validPasswords++
+	}
 }
 
 console.log(`Answer: ${validPasswords}`)
