b2d63abc48fd33e7bb39ed2ebccf57ef52bf9d15
Mitchell McCulloch <mitchell.mcculloch@rangle.io>
2019-12-18 13:27:34 -0500

    2019 - Day 5 updates that I'll need for future intcode business

--- 2019/day5/day5.js
+++ 2019/day5/day5.js
@@ -9,18 +9,18 @@ const intcodes = fs
 // must return back a result, that the while loop will set at the end if necessary
 const determineOperationResult = (opCode, modes, parameters) => {
 	let result = 0
-	if (opCode === '1') {
+	if (opCode === '01') {
 		if (modes[0] === 0) result += intcodes[parameters[0]]
 		else if (modes[0] === 1) result += parameters[0]
 		if (modes[1] === 0) result += intcodes[parameters[1]]
 		else if (modes[1] === 1) result += parameters[1]
-	} else if (opCode === '2') {
+	} else if (opCode === '02') {
 		result = 1
 		if (modes[0] === 0) result *= intcodes[parameters[0]]
 		else if (modes[0] === 1) result *= parameters[0]
 		if (modes[1] === 0) result *= intcodes[parameters[1]]
 		else if (modes[1] === 1) result *= parameters[1]
-	} else if (opCode === '5' || opCode === '6') {
+	} else if (opCode === '05' || opCode === '06') {
 		let num1, num2
 
 		if (modes[0] === 0) num1 = intcodes[parameters[0]]
@@ -28,25 +28,25 @@ const determineOperationResult = (opCode, modes, parameters) => {
 		if (modes[1] === 0) num2 = intcodes[parameters[1]]
 		else if (modes[1] === 1) num2 = parameters[1]
 
-		if (opCode === '5') {
+		if (opCode === '05') {
 			if (num1 !== 0) result = num2
 			else result = null
-		} else if (opCode === '6') {
+		} else if (opCode === '06') {
 			if (num1 === 0) result = num2
 			else result = null
 		}
 		console.log(`num1! ${num1} num2! ${num2}`)
-	} else if (opCode === '7' || opCode === '8') {
+	} else if (opCode === '07' || opCode === '08') {
 		let num1, num2
 		if (modes[0] === 0) num1 = intcodes[parameters[0]]
 		else if (modes[0] === 1) num1 = parameters[0]
 		if (modes[1] === 0) num2 = intcodes[parameters[1]]
 		else if (modes[1] === 1) num2 = parameters[1]
 
-		if (opCode === '7') {
+		if (opCode === '07') {
 			if (num1 < num2) result = 1
 			else result = 0
-		} else if (opCode === '8') {
+		} else if (opCode === '08') {
 			if (num1 === num2) result = 1
 			else result = 0
 		}
@@ -59,11 +59,15 @@ const determineOperationResult = (opCode, modes, parameters) => {
 let i = 0
 while (i < intcodes.length) {
 	let opCodeString = String(intcodes[i])
+	console.log(`OPCODE STRING ${opCodeString}`)
 	let indexIncrement
-	let opCode = String(opCodeString).slice(-1) // grab the last digit only
+	let opCode = String(opCodeString)
+		.slice(opCodeString.length - 2)
+		.padStart(2, '0')
+	console.log(`OPCODE STRING ${opCodeString}`)
 	let modes = []
 	let parameters = []
-	if (opCode === '1' || opCode === '2' || opCode === '7' || opCode === '8') {
+	if (opCode === '01' || opCode === '02' || opCode === '07' || opCode === '08') {
 		// 4 index instructions
 		parameters[0] = intcodes[i + 1]
 		parameters[1] = intcodes[i + 2]
@@ -72,12 +76,12 @@ while (i < intcodes.length) {
 		modes[0] = Number(opCodeString.charAt(opCodeString.length - 3)) || 0
 		modes[1] = Number(opCodeString.charAt(opCodeString.length - 4)) || 0
 		modes[2] = Number(opCodeString.charAt(opCodeString.length - 5)) || 0
-	} else if (opCode === '3' || opCode === '4') {
+	} else if (opCode === '03' || opCode === '04') {
 		// 2 index instructions
 		parameters[0] = intcodes[i + 1]
 		modes[0] = Number(opCodeString.charAt(opCodeString.length - 3)) || 0
 		indexIncrement = 2
-	} else if (opCode === '5' || opCode === '6') {
+	} else if (opCode === '05' || opCode === '06') {
 		parameters[0] = intcodes[i + 1]
 		parameters[1] = intcodes[i + 2]
 		modes[0] = Number(opCodeString.charAt(opCodeString.length - 3)) || 0
@@ -87,23 +91,25 @@ while (i < intcodes.length) {
 
 	console.log(`opcode ${opCode} modes ${modes} parameters ${parameters} i ${i} `)
 	let result
-	if (opCode === '1' || opCode === '2' || opCode === '7' || opCode === '8') {
+	if (opCode === '01' || opCode === '02' || opCode === '07' || opCode === '08') {
 		result = determineOperationResult(opCode, modes, parameters)
-	} else if (opCode === '3') {
+	} else if (opCode === '03') {
 		intcodes[parameters[0]] = 5 // Seems fine to hardcode this value instead of asking for user input
-	} else if (opCode === '4') {
-		if (modes[0] === 0) console.log(`DIAGNOSTIC OUTPUT: ${String(intcodes[parameters[0]])}`)
+	} else if (opCode === '04') {
+		if (modes[0] === 0)
+			console.log(`DIAGNOSTIC OUTPUT: ${String(intcodes[parameters[0]])}`)
 		else if (modes[0] === 1) console.log(`DIAGNOSTIC OUTPUT: ${String(parameters[0])}`)
-	} else if (opCode === '5' || opCode === '6') {
+	} else if (opCode === '05' || opCode === '06') {
 		const res = determineOperationResult(opCode, modes, parameters)
 		if (res) i = res
 		else i += 3
-	} else if (opCode === '9') {
+	} else if (opCode === '99') {
 		break
 	}
 
 	// opCode 3 and 4 are handled in the if blocks above
-	if (opCode === '1' || opCode === '2' || opCode === '7' || opCode === '8') intcodes[parameters[2]] = result
+	if (opCode === '01' || opCode === '02' || opCode === '07' || opCode === '08')
+		intcodes[parameters[2]] = result
 
 	i += indexIncrement
 }
